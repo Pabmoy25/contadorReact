453eb3041f6ec2500de266bb3cc25fc1b21b3da8
Pablo Moyano <pabmoy1725@gmail.com>
2023-12-12 16:00:51 -0300

    agregamos nuestro primer componente Informcion

---
+++ src/components/Informacion.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Informacion = () => {
+  //Aqui va la logica del componente
+
+  return (
+    //Aqui puede ir un poco de logica
+    <>
+      <h2>Conseptos de react</h2>
+      <section>
+        {/* Aqui va todo el maquetado del componente */}
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe?
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Informacion;
