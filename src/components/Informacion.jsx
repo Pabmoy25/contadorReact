886afa4850eac07a16a25dbc6153f662d00dc7c4
Pablo Moyano <pabmoy1725@gmail.com>
2023-12-14 15:13:05 -0300

    Agregamos un props en el componente Informacion

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-const Informacion = () => {
+const Informacion = (props) => {
   //Aqui va la logica del componente
 
   return (
     //Aqui puede ir un poco de logica
     <>
-      <h2>Conseptos de react</h2>
+      <h2>Conseptos de react {props.comision} </h2>
       <section>
         {/* Aqui va todo el maquetado del componente */}
 
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+          ratione? Tempore excepturi nesciunt dolor quia mollitia? Modi tempora
+          aut magnam molestiae numquam iure illum sunt eaque esse veritatis
+          explicabo incidunt excepturi quae quis eius, impedit doloribus ipsa at
+          officia beatae. Ut excepturi nihil expedita illo labore hic
+          consequatur deserunt quia! <br /> comision: {props.anioActualProps} 
         </p>
       </section>
     </>
